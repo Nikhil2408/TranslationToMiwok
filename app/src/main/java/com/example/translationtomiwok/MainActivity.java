7407abc1a46a610fdaa6dbcdb840d1718f56362b
Nikhil2408 <nikbansal1998@gmail.com>
2021-02-13 20:33:06 +0530

    Opens NumberActivity when clicked on Numbers TextView

--- app/src/main/java/com/example/translationtomiwok/MainActivity.java
+++ app/src/main/java/com/example/translationtomiwok/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.translationtomiwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersActivity(View view)
+    {
+        Intent intent=new Intent(this, NumberActivity.class);
+        startActivity(intent);
+    }
+
+    public void openFamilyMembersActivity(View view)
+    {
+
+    }
+
+    public void openColorsActivity(View view)
+    {
+
+    }
+
+    public void openPhrasesActivity(View view)
+    {
+
     }
 }

c2a5450fdffa89f645d1329d246c2f3b99186e6a
Nikhil2408 <nikbansal1998@gmail.com>
2021-02-14 22:14:47 +0530

    Added Explicit Intent and onClickListener Event Listener

--- app/src/main/java/com/example/translationtomiwok/MainActivity.java
+++ app/src/main/java/com/example/translationtomiwok/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,26 +14,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Event Listener implementation for NumberActivity
+        TextView numbersTextView=(TextView)findViewById(R.id.numbers);
+        numbersTextView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, NumberActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Event Listener implementation for FamilyActivity
+        TextView familyTextView =(TextView) findViewById(R.id.family);
+        familyTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Event Listener implementation for ColorsActivity
+        TextView  colorsTextView=(TextView) findViewById(R.id.colors);
+        colorsTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+               Intent intent=new Intent(MainActivity.this, ColorsActivity.class);
+               startActivity(intent);
+            }
+        });
+
+        //Event Listener implementation for PhrasesActivity
+        TextView phrasesTextView=(TextView)findViewById(R.id.phrases);
+        phrasesTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void openNumbersActivity(View view)
-    {
-        Intent intent=new Intent(this, NumberActivity.class);
-        startActivity(intent);
-    }
-
-    public void openFamilyMembersActivity(View view)
-    {
-
-    }
-
-    public void openColorsActivity(View view)
-    {
-
-    }
-
-    public void openPhrasesActivity(View view)
-    {
-
-    }
 }
--- app/src/main/java/com/example/translationtomiwok/NumberActivity.java
+++ app/src/main/java/com/example/translationtomiwok/NumberActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class NumberActivity extends AppCompatActivity {
-
+public class NumberActivity extends AppCompatActivity
+{
+    String numbers[] = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
+
     }
 }
